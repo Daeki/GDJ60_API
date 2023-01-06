5f51b5afdd7eb568cdfe8b57ec8f1011814ca375
Daeki <kimdaeki@gmail.com>
2023-01-06 10:46:03 +0900

    finish

--- src/com/iu/api1/strings/StringEx2.java
+++ src/com/iu/api1/strings/StringEx2.java
@@ -5,10 +5,31 @@ import java.util.Scanner;
 public class StringEx2 {
 
 	public static void main(String[] args) {
+		System.out.println("Start");
 		Scanner sc = new Scanner(System.in);
 		String [] names = {"jpg", "jpeg", "gif", "png"};
 		System.out.println("파일명을 입력");
-		String fileName=sc.next(); //sf.sd.dsf.df
+		String fileName=sc.next(); //ab.cdef.jpg
+		
+		//확장자 분리 작업
+		int idx = fileName.lastIndexOf(".");
+		String result = fileName.substring(idx+1);
+		System.out.println(result);
+		boolean flag=true;
+		//확장자가 이미지인지 검증
+		for(int i=0;i<names.length;i++) {
+			if(result.equals(names[i])) {
+				System.out.println("정상 파일이다");
+				i=names.length;
+				//flag = false;
+				flag=!flag;
+				break;
+			}
+		}
+		
+		if(flag) {
+			System.out.println("비정상 파일이다");
+		}
 		
 		
 		
@@ -16,6 +37,8 @@ public class StringEx2 {
 		
 		
 		
+		
+		System.out.println("Finish");
 
 	}
 
