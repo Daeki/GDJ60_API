374cba9bc29be729bb11ccc66bdfab9747c842df
Daeki <kimdaeki@gmail.com>
2023-01-10 11:57:42 +0900

    init

--- src/com/iu/api2/collections/ex1/StudentDAO.java
+++ src/com/iu/api2/collections/ex1/StudentDAO.java
@@ -1,5 +1,8 @@
 package com.iu.api2.collections.ex1;
 
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
 public class StudentDAO {
 	
 	private StringBuffer sb;
@@ -11,6 +14,30 @@ public class StudentDAO {
 		sb.append("winter-2-86-84-75-");
 		sb.append("suji, 3, 89, 74, 87 ");
 		sb.append("choa, 4, 71, 25, 99 ");
+	}
+	
+	//학생정보초기화
+	public ArrayList<StudentDTO>  init() {
+		String data = this.sb.toString();
+		data=data.replace(" ", "-");
+		data=data.replace(",", "");
+		
+		System.out.println(data);
+		StringTokenizer st = new StringTokenizer(data, "-");
+		ArrayList<StudentDTO> ar = new ArrayList<>();
+		while(st.hasMoreTokens()) {
+			StudentDTO studentDTO = new StudentDTO();
+			studentDTO.setName(st.nextToken());
+			studentDTO.setNum(Integer.parseInt(st.nextToken()));
+			studentDTO.setKor(Integer.parseInt(st.nextToken()));
+			studentDTO.setEng(Integer.parseInt(st.nextToken()));
+			studentDTO.setMath(Integer.parseInt(st.nextToken()));
+			studentDTO.setTotal(studentDTO.getKor()+studentDTO.getEng()+studentDTO.getMath() );
+			studentDTO.setAvg(studentDTO.getTotal()/3.0);
+			ar.add(studentDTO);
+		}
+		
+		return ar;
 	}
 
 }
